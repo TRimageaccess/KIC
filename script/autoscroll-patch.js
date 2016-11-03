e80483887f3fe5f2664c654dfe14f23af778a16e
ArtemNadiein <flick727nadein@gmail.com>
2016-11-03 15:28:09 +0200

    add analytics.js

--- script/autoscroll-patch.js
+++ script/autoscroll-patch.js
@@ -1,26 +1,26 @@
 var $j = jQuery.noConflict();
-$j(function() {
+$j(function () {
     SlideShowScroller.initialize();
 });
-var SlideShowScroller = (function() {
+var SlideShowScroller = (function () {
     var _scrollerApi;
     var _scrollerConfig;
     var _isPaused = false;
     var _timerInterval;
-    var initialize = function() {
+    var initialize = function () {
         configureScrollable();
         $j('a.play').bind('click', playButtonClickHandler);
         setTimeout(easingHandler, 4000);
         resetTimer();
         startTimer();
-    }
-    var startTimer = function() {
+    };
+    var startTimer = function () {
         setInterval(autoPlayHandler, 4000);
-    }
-    var resetTimer = function() {
+    };
+    var resetTimer = function () {
         _timerInterval = 8;
-    }
-    var configureScrollable = function() {
+    };
+    var configureScrollable = function () {
         /*var root = $j("div#Feature").scrollable({*/
         var root = $j(".f5-pane > .scrollable").scrollable({
             easing: 'easeInOutQuart',
@@ -30,17 +30,17 @@ var SlideShowScroller = (function() {
             circular: true,
             onSeek: onSeekHandler
         });
-        $j.easing.easeInOutQuart = function(x, t, b, c, d) {
+        $j.easing.easeInOutQuart = function (x, t, b, c, d) {
             if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
             return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
-        }
+        };
         _scrollerApi = root.data("scrollable");
         _scrollerConfig = _scrollerApi.getConf();
-    }
-    var onSeekHandler = function() {
+    };
+    var onSeekHandler = function () {
         resetTimer();
-    }
-    var playButtonClickHandler = function() {
+    };
+    var playButtonClickHandler = function () {
         if (_isPaused) {
             _isPaused = false;
         } else {
@@ -48,20 +48,20 @@ var SlideShowScroller = (function() {
         }
         resetTimer();
         $j(this).toggleClass('hover');
-    }
-    var autoPlayHandler = function() {
+    };
+    var autoPlayHandler = function () {
         if (_isPaused) return;
         _timerInterval--;
         if (_timerInterval < 1) {
             _scrollerApi.next();
             resetTimer();
         }
-    }
-    var easingHandler = function() {
+    };
+    var easingHandler = function () {
         _scrollerApi.move(6);
         _scrollerConfig.easing = 'swing';
         _scrollerConfig.speed = 800;
-    }
+    };
     return {
         initialize: initialize
     }
--- script/include.js
+++ script/include.js
@@ -2,28 +2,24 @@ $(document).ready(function () {
 // include header and footer
 
     $("#header").load("blocks/header.html");
-    $("#footer").load("blocks/footer.html");
+    $("#footer").load("blocks/footer.html", function () {
+        var numRand = Math.floor(Math.random() * 72);
+        var newsTicker = $("#newsTicker");
+        newsTicker.smoothDivScroll({
+            autoScrollingMode: "onStart",
+            autoScrollingDirection: "endlessLoopRight",
+            autoScrollingStep: 1,
+            autoScrollingInterval: 10,
+            startAtElementId: "QID" + numRand
+        });
 
-
-//news ticker
-
-    var numRand = Math.floor(Math.random() * 72);
-    var newsTicker = $("#newsTicker");
-    newsTicker.smoothDivScroll({
-        autoScrollingMode: "onStart",
-        autoScrollingDirection: "endlessLoopRight",
-        autoScrollingStep: 1,
-        autoScrollingInterval: 10,
-        startAtElementId: "QID" + numRand
+        newsTicker.bind("mouseover", function () {
+            newsTicker.smoothDivScroll("stopAutoScrolling");
+        });
+        newsTicker.bind("mouseout", function () {
+            newsTicker.smoothDivScroll("startAutoScrolling");
+        });
     });
-
-    newsTicker.bind("mouseover", function () {
-        newsTicker.smoothDivScroll("stopAutoScrolling");
-    });
-    newsTicker.bind("mouseout", function () {
-        newsTicker.smoothDivScroll("startAutoScrolling");
-    });
-
 
     $("ul.tabs").tabs("div.panes > div");
 
