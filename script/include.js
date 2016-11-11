84aed5fcf60966f2cb70bb0d3bc44da2488a2cf1
bogdan <mykytenko.bohdan@gmail.com>
2016-11-11 14:10:21 +0200

    share social media

--- script/include.js
+++ script/include.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 // include header and footer
-
     $("#header").load("blocks/header.html");
     $("#footer").load("blocks/footer.html", function () {
         var numRand = Math.floor(Math.random() * 72);
@@ -22,5 +21,17 @@ $(document).ready(function () {
     });
 
     $("ul.tabs").tabs("div.panes > div");
+
+    var desc = $('meta[name=description]').attr('content');
+
+    $(".share").hideshare({
+        decription:'"' + desc + '"',
+        media:'http://upload.ted-kteam.com/KIC/images/DSLG-and-IA-Logos.png',
+        facebook: true,
+        twitter: false,
+        pinterest: false,
+        googleplus: true,
+        linkedin: true
+    });
 
 });
